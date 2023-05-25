ad5ad02ffbe8057f8b64daa1ef989e8740c83ece
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-05-25 21:31:18 +0300

    first commit

---
+++ arcticle/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField( max_length=50)
+    image =  models.ImageField(_(""), upload_to='category/images/')
+
+    def __str__(self):
+        return self.name
+    
+
+class Arcticle(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='article/images/')
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    read = models.BooleanField()
+    
+    def __str__(self):
+        return self.title
+    
